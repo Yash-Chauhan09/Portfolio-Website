d17b885524fbf2ce79442953ab3a4b47aa541b64
Yash <you@example.com>
2021-01-05 22:56:35 +0530

    db connected

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/portfolio-messages", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/portfolio-messages", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
