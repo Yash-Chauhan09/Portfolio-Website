92dbadbfe60ab70d789a205c650b51d22668e5d6
Yash <you@example.com>
2021-01-06 12:09:02 +0530

    commited

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+// const DATABASE_URL = 'mongodb://yashchauhan:mymessagesport@portfolio-message-shard-00-00.kliut.mongodb.net:27017,portfolio-message-shard-00-01.kliut.mongodb.net:27017,portfolio-message-shard-00-02.kliut.mongodb.net:27017/portfolio-messages?ssl=true&replicaSet=atlas-3cf4r0-shard-0&authSource=admin&retryWrites=true&w=majority'
+
 mongoose.connect(process.env.DATABASE_URL || "mongodb://localhost:27017/portfolio-messages", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -9,3 +11,5 @@ mongoose.connect(process.env.DATABASE_URL || "mongodb://localhost:27017/portfoli
 }).catch((e) =>{
     console.log(e)
 });
+
+// mymessagesport
